81b12e48d1920c880c23b6bec9afe8967792218c
MinexAutomation <minex.automation@gmail.com>
2020-05-17 02:20:05 -0700

    Improved IMessageSink extensions.

--- source/R5T.D0010.Extensions/Code/Services/Extensions/IMessageSinkExtensions.cs
+++ source/R5T.D0010.Extensions/Code/Services/Extensions/IMessageSinkExtensions.cs
@@ -2,24 +2,28 @@
 using System.Threading.Tasks;
 
 using R5T.D0001;
+using R5T.T0001;
 
 
 namespace R5T.D0010
 {
     public static class IMessageSinkExtensions
     {
-        public static async Task AddErrorMessageAsync(this IMessageSink messageSink, INowUtcProvider nowUtcProvider, string errorMessage)
+        public static async Task AddMessageAsync(this IMessageSink messageSink, INowUtcProvider nowUtcProvider, MessageType messageType, string message)
         {
             var timestampUtc = await nowUtcProvider.GetNowUtcAsync();
 
-            await messageSink.AddErrorMessageAsync(timestampUtc, errorMessage);
+            await messageSink.AddMessageAsync(timestampUtc, messageType, message);
         }
 
-        public static async Task AddOutputMessageAsync(this IMessageSink messageSink, INowUtcProvider nowUtcProvider, string errorMessage)
+        public static Task AddErrorMessageAsync(this IMessageSink messageSink, INowUtcProvider nowUtcProvider, string errorMessage)
         {
-            var timestampUtc = await nowUtcProvider.GetNowUtcAsync();
+            return messageSink.AddMessageAsync(nowUtcProvider, MessageType.Error, errorMessage);
+        }
 
-            await messageSink.AddOutputMessageAsync(timestampUtc, errorMessage);
+        public static Task AddOutputMessageAsync(this IMessageSink messageSink, INowUtcProvider nowUtcProvider, string outputMessage)
+        {
+            return messageSink.AddMessageAsync(nowUtcProvider, MessageType.Output, outputMessage);
         }
     }
 }
